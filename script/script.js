793711035e3d918c5e8c8c735ca94b18bc6c241d
Dmitry Zhilin <dmitry.zhilin@list.ru>
2023-11-14 15:30:07 +0700

    123

--- script/script.js
+++ script/script.js
@@ -6,3 +6,22 @@ jQuery(document).ready(function () {
       return false;
    });
 });
+
+function myFunction() {
+   document.getElementById("floatingBlock").classList.toggle("show");
+   document.getElementById("floatingNavbar").classList.toggle("show");
+};
+
+window.onclick = function (e) {
+   if (!e.target.matches('.floating_navbar>img')) {
+      var floatingBlock = document.getElementById("floatingBlock");
+      if (floatingBlock.classList.contains('show')) {
+         floatingBlock.classList.remove('show');
+      }
+      var floatingNavbar = document.getElementById("floatingNavbar");
+      if (floatingNavbar.classList.contains('show')) { }
+      else (floatingNavbar.classList.add('show'))
+   }
+};
+
+
